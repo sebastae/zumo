fe2c397819c124ece333f464ed3d8dd8ecb4e91f
Sebastian Ellefsen <sebastae@stud.ntnu.no>
2018-03-14 13:24:17 +0100

    Start of bluetooth

---
+++ bt.ino
@@ -0,0 +1,47 @@
+
+template <struct T>
+class BTParam{
+  private:
+  T* var;
+
+  public:
+
+  void set(T val){
+    *var = val;
+  }
+
+  T get(){
+    return *var;
+  }
+}
+
+class BluetoothParameters{
+  private:
+
+  struct dtypes{
+    int i;
+    String s;
+    boolean b;
+    double d;
+  }
+
+  map<String, dtypes> paramMap;
+
+  public:
+  
+  BluetoothParameters(){
+      
+  }
+
+  
+  
+}
+
+
+void setup() {
+    
+}
+
+void loop() {
+
+}
