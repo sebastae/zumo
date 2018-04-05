f9c85e6dc09032a1e3d012524ee2781908c1d674
Sebastian Ellefsen <sebastae@stud.ntnu.no>
2018-04-05 15:02:18 +0200

    Request handling

--- bt/bt.ino
+++ bt/bt.ino
@@ -52,10 +52,40 @@ class BTRequest{
         method = GET;
         break;
       default:
-        
+        method = GETALL;
+        Serial.println(requestString[0] + " is not a recognized request type, defaulting to GETALL");
+        break;
     };
+
+    if(method == GETALL){
+      return;
+    } else{
+      String reqStr = requestString.substring(1);
+      String dtype = reqStr.substring(0,3);
+
+      int colonPos = reqStr.indexOf(':');
+
+      String varName = reqStr.substring(2, colonPos-3);
+      String varVal = reqStr.substring(colonPos+1);
+
+     varVal = varVal.substring(0, varVal.length());
+
+
+     if(dtype == "INT"){
+      type = INT;
+     } else if(dtype == "DBL") {
+      type = DBL;
+     } else if(dtype == "STR"){
+      type = STR;
+     }
+
+
+     varname = varName;
+     value = varVal;
+    }
+
+
   };
-  
 };
 
 class BluetoothParameters{
