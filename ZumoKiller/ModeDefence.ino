b84500633cbba537cf28b8373f862e45495e33b9
Realfsen <32299760+Realfsen@users.noreply.github.com>
2018-04-06 13:23:29 +0200

    Added defenee mode

--- ZumoKiller/ModeDefence.ino
+++ ZumoKiller/ModeDefence.ino
@@ -1,3 +1,19 @@
-void modeDefence() {  
-  motors.setSpeeds(motorMaxSpeed, motorMaxSpeed); 
+void modeDefence() {
+  //Lang avstand
+  if(checkUSdistance('B') >= usSenTrigDefThreshold){
+    motors.setSpeeds(motorMaxSpeed, -motorMaxSpeed);
+    delay(TURNTIME);
+    mode = ATTACK;
+  }
+
+  //Kort avstand
+  else if (checkUSdistance('B') < usSenTrigDefThreshold){
+   motors.setSpeeds(-motorMaxSpeed, -motorMaxSpeed);  
+  }
+
+  else {
+    motors.setSpeeds(-motorMaxSpeed, motorMaxSpeed);
+    delay(TURNTIME);
+    mode = ATTACK;
+  }
 }
