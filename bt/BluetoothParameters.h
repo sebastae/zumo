6eb79ffc0cfd6c34292b0761a8dee2e41f4362d8
Sebastian Ellefsen <sebastae@stud.ntnu.no>
2018-04-06 14:19:27 +0200

    Request handling finished, sending maybe done?

--- bt/BluetoothParameters.h
+++ bt/BluetoothParameters.h
@@ -3,7 +3,7 @@
 
 // Comment our to turn off debugging
 #ifndef BT_DEBUG
-#define BT_DEBUG
+//#define BT_DEBUG
 #endif
 
 #ifndef BTPRMS_H
@@ -66,6 +66,7 @@ class BTRequest{
 class BluetoothManager{
   private:
     BluetoothParameters* btParams;
+    String reqStr;
   public:
     BluetoothManager(BluetoothParameters* btParams);
     void receive();
@@ -82,6 +83,9 @@ class BluetoothParameters{
     BluetoothParameters();
     void bindBTManager(BluetoothManager* mng);
     void handleRequest(String request);
+    void add(int* i, String name);
+    void add(double* d, String name);
+    void add(String* s, String name);
 };
 
 
@@ -123,7 +127,7 @@ BTRequest::BTRequest(String requestString){
       method = GETALL;
 
       #ifdef BT_DEBUG
-        Serial.println(requestString[0] + " is not a recognized request type, defaulting to GETALL");
+        Serial.println((char) requestString[0] + " is not a recognized request type, defaulting to GETALL");
       #endif
       break;
   };
@@ -131,13 +135,19 @@ BTRequest::BTRequest(String requestString){
   if(method == GETALL){
     return;
   } else{
-    String reqStr = requestString.substring(1);
-    String dtype = reqStr.substring(0,3);
+    #ifdef BT_DEBUG
+      Serial.println(requestString);
+    #endif
 
-    int colonPos = reqStr.indexOf(':');
+    String tinfo = requestString.substring(0,4);
+    String pinfo = requestString.substring(4);
 
-    String varName = reqStr.substring(2, colonPos-3);
-    String varVal = reqStr.substring(colonPos+1);
+    String dtype = tinfo.substring(1);
+
+    int colonPos = pinfo.indexOf(':');
+
+    String varName = pinfo.substring(0,colonPos);
+    String varVal = pinfo.substring(colonPos+1);
 
    varVal = varVal.substring(0, varVal.length()-1);
 
@@ -178,21 +188,21 @@ String BTRequest::getValue(){
 BluetoothManager::BluetoothManager(BluetoothParameters* btParams) : btParams(btParams){};
 
 void BluetoothManager::receive(){
-  String requestString = "";
   while(Serial.available()){
     char receivedByte = Serial.read();
-    requestString += receivedByte;
+    this->reqStr.concat(receivedByte);
     if(receivedByte == ';'){
-      this->btParams->handleRequest(requestString);
-      requestString = "";
+      #ifdef BT_DEBUG
+        Serial.println(this->reqStr);
+      #endif
+      this->btParams->handleRequest(this->reqStr);
+      this->reqStr = "";
     };
   };
 };
 
 void BluetoothManager::send(String requestString){
-  #ifdef BT_DEBUG
-    Serial.println(requestString);
-  #endif
+  Serial.print(requestString);
 };
 
 
@@ -204,6 +214,31 @@ BluetoothParameters::BluetoothParameters(){
   doubleMap = std::map<String, BTParam<double>>();
 };
 
+void BluetoothParameters::add(int* i, String name){
+  if(this->intMap.find(name) == this->intMap.end()){
+
+    BTParam<int> prm = BTParam<int>(i);
+
+    this->intMap.insert(std::make_pair(name, prm)); 
+  }
+};
+
+void BluetoothParameters::add(double* d, String name){
+  if(this->doubleMap.find(name) == this->doubleMap.end()){
+    BTParam<double> prm = BTParam<double>(d);
+    this->doubleMap.insert(std::make_pair(name, prm));
+  }
+};
+
+void BluetoothParameters::add(String* s, String name){
+  if(this->stringMap.find(name) == this->stringMap.end()){
+    BTParam<String> prm = BTParam<String>(s);
+
+    this->stringMap.insert(std::make_pair(name, prm));
+    
+  }
+};
+
 void BluetoothParameters::bindBTManager(BluetoothManager* mng){
   this->btMng = mng;
 }
@@ -212,8 +247,113 @@ void BluetoothParameters::handleRequest(String request){
   BTRequest req = BTRequest(request);
 
   if(req.getMethod() == GETALL){
+    #ifdef BT_DEBUG
+      Serial.println("GETALL");    
+    #endif
+
+    String str;
+
+    auto intIter = this->intMap.begin();
+    while(intIter != this->intMap.end()){
+      str += "GINT";
+      str +=  intIter->first;
+      str += ":";
+      str += String(intIter->second.get());
+      str += ";";
+
+      intIter++;
+    }
+
+    auto dblIter = this->doubleMap.begin();
+    while(dblIter != this->doubleMap.end()){
+      str += "GDBL";
+      str += dblIter->first;
+      str += ":";
+      str += String(dblIter->second.get());
+      str += ";";
+
+      dblIter++;
+    }
+
+    auto strIter = this->stringMap.begin();
+    while(strIter != this->stringMap.end()){
+      str += "GSTR";
+      str += strIter->first;
+      str += ":";
+      str += strIter->second.get();
+      str += ";";
+
+      strIter++;
+    }
+
+    this->btMng->send(str);
+        
+  } else if(req.getMethod() == GET){
+    #ifdef BT_DEBUG
+      Serial.println("GET");
+    #endif
+
+    String sStr = "G";
+    String val = "";
+
+    if(req.getType() == INT){
+      sStr += "INT";
+      val = this->intMap.find(req.getName()) != this->intMap.end() ? String(this->intMap.find(req.getName())->second.get()) : "";
+    } else if (req.getType() == DBL){
+      sStr += "DBL";
+      val = this->doubleMap.find(req.getName()) != this->doubleMap.end() ? String(this->doubleMap.find(req.getName())->second.get()) : "";
+    } else if (req.getType() == STR){
+      sStr += "STR";
+      val = this->stringMap.find(req.getName()) != this->stringMap.end() ? this->stringMap.find(req.getName())->second.get() : "";
+    }
+
+    sStr += req.getName();
+    sStr += ":";
+    sStr += val;
+    sStr += ";";
+
+    this->btMng->send(sStr);
     
-  }
+  } else if(req.getMethod() == SET){
+    #ifdef BT_DEBUG
+      Serial.println("SET");
+    #endif
+    if(req.getType() == INT){
+      auto search = this->intMap.find(req.getName());
+
+      if(search != this->intMap.end()){
+        search->second.set(req.getValue().toInt());
+      } else {
+        #ifdef BT_DEBUG
+          Serial.println("Key \"" + req.getName() + "\" was not found in the int map");
+        #endif
+      }
+    } else if(req.getType() == DBL){
+      auto search = this->doubleMap.find(req.getName());
+
+      if(search != this->doubleMap.end()){
+        search->second.set(req.getValue().toDouble());
+      } else {
+        #ifdef BT_DEBUG
+          Serial.println("Key \"" + req.getName() + "\" was not found in the double map");
+        #endif
+      }
+    } else if(req.getType() == STR){
+      auto search = this->stringMap.find(req.getName());
+
+      if(search != this->stringMap.end()){
+        search->second.set(req.getValue());
+      } else {
+        #ifdef BT_DEBUG
+          Serial.println("Key \"" + req.getName() + "\" was not found in the string map");
+        #endif
+      }
+    } else {
+      #ifdef BT_DEBUG
+        Serial.println("Datatype not supported");
+      #endif
+    }
+  };
 };
 
 #endif
--- bt/bt.ino
+++ bt/bt.ino
@@ -1,11 +1,28 @@
 #include "BluetoothParameters.h"
 
+BluetoothParameters btParams = BluetoothParameters();
+BluetoothManager btManager = BluetoothManager(&btParams);
 
 
+int a = 0;
+String b = "";
+
+int aPrev = 0;
+String bPrev = "";
 void setup() {
-    
+    Serial.begin(9600);
+    btParams.bindBTManager(&btManager);
+    btParams.add(&a, "a");
+    btParams.add(&b, "b");
 }
 
 void loop() {
-
+  btManager.receive();
+  if(a!= aPrev || b != bPrev){
+    Serial.print(a);
+    Serial.print(" , ");
+    Serial.println(b);
+   aPrev = a;
+   bPrev = b;
+  }
 }
